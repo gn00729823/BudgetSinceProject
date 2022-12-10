47294a497133f574db46698bfc8440c522c5d10e
DESKTOP-NC4FCA6\YileUser <gn00729823@gmail.com>
2022-12-10 16:00:45 +0800

    commit

--- BudgetSinceProject/UnitTest1.cs
+++ BudgetSinceProject/UnitTest1.cs
@@ -1,18 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices.ComTypes;
 using NUnit.Framework;
-
+using NSubstitute;
 namespace BudgetSinceProject
 {
     public class Tests
     {
+        private BudgetService _budgetService;
+        private IbudgetRepo budgetRepo;
         [SetUp]
         public void Setup()
         {
+            budgetRepo = new TestBudgetRepo();
+            _budgetService = new BudgetService(budgetRepo);
         }
 
         [Test]
-        public void Test1()
+        public void start_bigger_Then_end()
         {
-            Assert.Pass();
+         var budget=   _budgetService.Query(DateTime.Today, DateTime.MinValue);
+           Assert.AreEqual(0,budget);
         }
+
+        private class TestBudgetRepo : IbudgetRepo
+        {
+            public List<Budget> GetAll()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+
+    public class BudgetService
+    {
+        private IbudgetRepo _budgetRepo;
+
+        public BudgetService(IbudgetRepo budgetRepo)
+        {
+
+        }
+
+
+        public double Query(DateTime start, DateTime end)
+        {
+            var budgetList = _budgetRepo.GetAll();
+            if (start > end)
+                return 0;
+
+            return 0;
+        }
+    }
+
+
+
+    public interface IbudgetRepo
+    {
+        List<Budget> GetAll();
+    }
+
+    public class Budget
+    {
+        public string yearMonth;
+        public int amount;
     }
 }
